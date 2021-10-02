3130038a8f993b6988209f380de2ff80f1311d31
kirknaoborot <drishik05@gmail.com>
2021-10-02 16:33:17 +0300

    Add ServiceExtension

--- AdminPanelService.Rest/Startup.cs
+++ AdminPanelService.Rest/Startup.cs
@@ -1,15 +1,10 @@
+using AdminPanelService.Service;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace AdminPanelService.Rest
 {
@@ -26,6 +21,7 @@ namespace AdminPanelService.Rest
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddService();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ AdminPanelService.Service/ServiceExtension.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace AdminPanelService.Service
+{
+	public static class ServiceExtension
+	{
+		public static void AddService(this IServiceCollection services)
+		{
+		}
+	}
+}
